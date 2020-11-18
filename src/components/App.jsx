4ed85fab6053048518fad39155347ab6f7225b08
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-18 12:47:00 +0100

    Add logic in components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+    setNotes(prevNotes =>{
+      return [...prevNotes, newNote];
+    })
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((item, index) =>{
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+        onAdd={addNote}
+       />
+       {
+          notes.map((noteItem, index)=>
+          <Note 
+            key={index} 
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onChecked={deleteNote}
+            />
+          )
+        }
       <Footer />
     </div>
   );
